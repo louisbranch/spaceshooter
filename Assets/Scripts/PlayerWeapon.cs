92396d5904250b8467ef4c12d5ff6287f2e39255
Luiz Branco <me@luizbranco.com>
2015-02-22 23:21:56 -0500

    Add shooting controller

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerWeapon : MonoBehaviour {
 
 	public float fireRate = 3.0f;
+	public float laserSpeed = 3.0f;
+
+	public GameObject projectile;
 
 	private float nextFire = 0f;
 
@@ -11,7 +14,10 @@ public class PlayerWeapon : MonoBehaviour {
 		bool fire = Input.GetButton("Fire1");
 		if (fire && (Time.time > nextFire)) {
 			nextFire = Time.time + fireRate;
-			Debug.Log ("FIRE!");
+			Vector3 position = transform.position;
+			position.y -= 0.03f;
+			GameObject laser = (GameObject)Instantiate(projectile, position, Quaternion.identity);
+			laser.rigidbody2D.velocity = Vector3.right * laserSpeed;
 		} 
 	}
 
