9194da2e94142e08df60c68f8c52344b79562a5e
Luiz Branco <me@luizbranco.com>
2015-02-23 10:50:46 -0500

    Spawn enemy projectiles

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public float laserSpeed = 4.5f;
+
+	public GameObject projectile;
+
+	private float nextFire = 0f;
+
+	private void Update () {
+		if (Time.time > nextFire) {
+			nextFire = Time.time + Random.Range(1,5);
+			Vector3 position = transform.position;
+			position.y -= 0.03f;
+			GameObject laser = (GameObject)Instantiate(projectile, position, Quaternion.identity);
+			laser.rigidbody2D.velocity = Vector3.left * laserSpeed;
+		}
+	}
+
+}
