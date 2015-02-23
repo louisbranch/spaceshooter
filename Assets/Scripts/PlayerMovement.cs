fe5f8024fb76f1403b973f377ad7be0e50efed9d
Luiz Branco <me@luizbranco.com>
2015-02-22 23:00:32 -0500

    Initial player movement code

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	private void Update() {
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+
+		if (vMove != 0) {
+			transform.Translate(Vector3.up * speed * Mathf.Sign(vMove) * Time.deltaTime);
+		} 
+
+		if (hMove != 0) {
+			transform.Translate(Vector3.right * speed * Mathf.Sign(hMove) * Time.deltaTime);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapon : MonoBehaviour {
+
+	public float fireRate = 3.0f;
+
+	private float nextFire = 0f;
+
+	private void Update () {
+		bool fire = Input.GetButton("Fire1");
+		if (fire && (Time.time > nextFire)) {
+			nextFire = Time.time + fireRate;
+			Debug.Log ("FIRE!");
+		} 
+	}
+
+}
