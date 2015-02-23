b36581a0a8867b118651c22416b419e2c5261515
Luiz Branco <me@luizbranco.com>
2015-02-23 17:38:39 -0500

    Add laser animations

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,13 +5,25 @@ public class Projectile : MonoBehaviour {
 
 	public string target;
 
+	Animator anim;
+
+	private void Awake () {
+		anim = GetComponent<Animator>();
+	}
+
 	private void OnTriggerEnter2D (Collider2D coll) {
 		if (coll.tag == target) {
 			Transform[] allChildren =  coll.GetComponentsInChildren<Transform>();
 			foreach (Transform child in allChildren) {
 				Destroy(child.gameObject);
 			}
-			Destroy(this);
+			Destroy(coll.gameObject);
+			anim.SetTrigger("Explode");
+			rigidbody2D.velocity = Vector3.zero;
 		}
+	}
+
+	public void DestroyProjectile () {
+		Destroy(this.gameObject);
 	}
 }
