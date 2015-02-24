405c72c2b4cf3fbaf8f753376bc9395eca1fbbf7
Luiz Branco <me@luizbranco.com>
2015-02-24 13:15:53 -0500

    Spawn powerup in a random area

---
+++ Assets/Scripts/PowerupSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupSpawner : MonoBehaviour {
+
+	public GameObject[] powerups;
+
+	public float spawnSpeed = 10f;
+
+	private float nextSpawn = 0;
+
+	private void Update () {
+		// spawn a new powerup every 10s around the spawner point
+		if (Time.time > nextSpawn) {
+			GameObject powerup = powerups[Random.Range(0, powerups.Length - 1)];
+			Vector2 position;
+			position.x = transform.position.x + Random.Range(-1f, 1f);
+			position.y = transform.position.y + Random.Range(-1f, 1f);
+			nextSpawn = Time.time + spawnSpeed;
+			GameObject laser = (GameObject)Instantiate(powerup, position, transform.rotation);
+		}
+	}
+
+}
